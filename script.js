1ec2121fff70e6597a316100340fa747680aab77
tareq-360 <mdtareqhossain2002@gmail.com>
2026-03-26 16:32:29 +0600

    all, open, closed card update done

--- script.js
+++ script.js
@@ -16,36 +16,76 @@ document.getElementById("signInBtn").addEventListener("click", () => {
     else {
         userName.value = "";
         password.value = "";
-        const wrongInfo= document.getElementById("wrongInfo");
+        const wrongInfo = document.getElementById("wrongInfo");
         wrongInfo.classList.remove("hidden");
         return;
     }
 
 })
 
-const allIssues=()=>{
-    const url= fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
-    .then(res=>res.json())
-    .then(data=>displayIssues(data.data));
+const issues = document.getElementById("issuesCount");
+const card = document.getElementById("card");
+// button active status manage
+
+const allBtn = document.getElementById("allBtn");
+const openBtn = document.getElementById("openBtn");
+const closedBtn = document.getElementById("closedBtn");
+const removeActiveClass = () => {
+
+    // allBtn.classList.add("btnDeactive");
+    openBtn.classList.add("btnDeactive");
+    closedBtn.classList.add("btnDeactive");
+    if (allBtn.addEventListener("click", () => {
+        allBtn.classList.remove("btnDeactive");
+        openBtn.classList.add("btnDeactive");
+        closedBtn.classList.add("btnDeactive");
+        console.log("all Button Clicked");
+
+    }));
+    else if (openBtn.addEventListener("click", () => {
+        openBtn.classList.remove("btnDeactive");
+        allBtn.classList.add("btnDeactive");
+        closedBtn.classList.add("btnDeactive");
+        console.log("Open Button Clicked");
+
+    }));
+    else if (closedBtn.addEventListener("click", () => {
+        closedBtn.classList.remove("btnDeactive");
+        allBtn.classList.add("btnDeactive");
+        openBtn.classList.add("btnDeactive");
+        console.log("Closed Button Clicked");
+
+    }));
+
+}
+removeActiveClass();
+
+const allIssues = () => {
+    const url = fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
+        .then(res => res.json())
+        .then(data => {
+            displayIssues(data.data);
+            openIssues(data.data);
+        });
 
 }
 
-const displayIssues=(issue)=>{
+const displayIssues = (issue) => {
     // console.log();
-    const totalIssue=issue.length;
-    const issues=document.getElementById("issuesCount");
-    issues.innerText=`${totalIssue} Issues`;
-    card.innerHTML="";
+    const totalIssue = issue.length;
+    // const issues = document.getElementById("issuesCount");
+    issues.innerText = `${totalIssue} Issues`;
+    card.innerHTML = "";
     issue.forEach(data => {
-        
+
         // console.log(data.id);
-        const card=document.getElementById("card");
-        
-        const newElement=document.createElement("div");
-        newElement.innerHTML=`
+
+
+        const newElement = document.createElement("div");
+        newElement.innerHTML = `
             <div class="card bg-white rounded-md shadow-md p-5">
                     <div class="flex  justify-between space-y-5">
-                        <img class="w-10 h-10 ${(data.status=="open") ? "bg-green-400" : "bg-red-300"} p-2 rounded-full" src="${(data.status=="open") ? "./assets/Open-Status.png" : "./assets/Closed- Status .png"}" alt="">
+                        <img class="w-10 h-10 ${(data.status == "open") ? "bg-green-400" : "bg-red-300"} p-2 rounded-full" src="${(data.status == "open") ? "./assets/Open-Status.png" : "./assets/Closed- Status .png"}" alt="">
                         <p class="btn btn-primary">${data.priority}</p>
                     </div>
                     <div class=" space-y-4">
@@ -65,5 +105,87 @@ const displayIssues=(issue)=>{
         card.append(newElement);
 
     });
+}
+
+const openIssues = (issue) => {
+    // console.log(issue);
+    allBtn.addEventListener("click",()=>{
+        allIssues();
+    })
+    openBtn.addEventListener("click", () => {
+        let i = 0;
+        card.innerHTML = "";
+        issue.forEach(data => {
+            // console.log(open.status);
+            if (data.status == "open") {
+                i++;
+                const newElement = document.createElement("div");
+                newElement.innerHTML = `
+            <div class="card bg-white rounded-md shadow-md p-5">
+                    <div class="flex  justify-between space-y-5">
+                        <img class="w-10 h-10 ${(data.status == "open") ? "bg-green-400" : "bg-red-300"} p-2 rounded-full" src="${(data.status == "open") ? "./assets/Open-Status.png" : "./assets/Closed- Status .png"}" alt="">
+                        <p class="btn btn-primary">${data.priority}</p>
+                    </div>
+                    <div class=" space-y-4">
+                        <h3 class="font-bold">${data.title}</h3>
+                        <p>${data.description}</p>
+                        <div class="flex space-x-2">
+                            <p class="bg-red-200 p-1 rounded-lg">BUG</p>
+                            <p class="bg-red-200 p-1 rounded-lg">HELP WANTED</p>
+                        </div>
+                        <hr>
+                        <p>#1 by ${data.author}</p>
+                        <p>${data.createdAt}</p>
+
+                    </div>
+                </div>
+        `
+                card.append(newElement);
+
+            }
+        })
+        // console.log(i);
+
+        issues.innerText = `${i} Issues`;
+    })
+    closedBtn.addEventListener("click", () => {
+        let i = 0;
+        card.innerHTML = "";
+        issue.forEach(data => {
+            // console.log(open.status);
+            if (data.status == "closed") {
+                i++;
+                const newElement = document.createElement("div");
+                newElement.innerHTML = `
+            <div class="card bg-white rounded-md shadow-md p-5">
+                    <div class="flex  justify-between space-y-5">
+                        <img class="w-10 h-10 ${(data.status == "open") ? "bg-green-400" : "bg-red-300"} p-2 rounded-full" src="${(data.status == "open") ? "./assets/Open-Status.png" : "./assets/Closed- Status .png"}" alt="">
+                        <p class="btn btn-primary">${data.priority}</p>
+                    </div>
+                    <div class=" space-y-4">
+                        <h3 class="font-bold">${data.title}</h3>
+                        <p>${data.description}</p>
+                        <div class="flex space-x-2">
+                            <p class="bg-red-200 p-1 rounded-lg">BUG</p>
+                            <p class="bg-red-200 p-1 rounded-lg">HELP WANTED</p>
+                        </div>
+                        <hr>
+                        <p>#1 by ${data.author}</p>
+                        <p>${data.createdAt}</p>
+
+                    </div>
+                </div>
+        `
+                card.append(newElement);
+
+            }
+        })
+        // console.log(i);
+
+        issues.innerText = `${i} Issues`;
+    })
+
+
+
 }
 allIssues();
